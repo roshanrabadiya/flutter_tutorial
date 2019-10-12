09b5bf539c17b393e3eafe83791ea271b3255573
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-12 20:00:44 +0530

    Create row_and_column_widget.dart

---
+++ height_width_font/lib/app_screens/row_and_column_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            padding: EdgeInsets.only(top: 40.0,left: 10.0,right: 10.0),
+            alignment: Alignment.center,
+            color: Colors.blue,
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Flutter",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: 'Grenze',
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "Welcome to Fuutter Tutorial For Bignners",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: 'Grenze',
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white),
+                  )),
+                ]),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Dart",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 25.0,
+                            fontFamily: 'Grenze',
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        "Welcome to Dart Tutorial For Bignners",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 25.0,
+                            fontFamily: 'Grenze',
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                ])
+              ],
+            )));
+  }
+}
