c2a50a54bb1feb5cc03f3ae627cea3c544c7e6a1
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-12 18:47:25 +0530

    Create main.dart

---
+++ height_width_font/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_first_app/app_screens/Home.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Exploring UI Widgets",
+    home: Home(),
+  ));
+}
+
